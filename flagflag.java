b73ca25e9b4ca8eb1eea410e0901b109ec8b3441
xyang399 <35610008+xyang399@users.noreply.github.com>
2021-02-07 20:55:55 -0600

    flag-flag

---
+++ flagflag.java
@@ -0,0 +1,41 @@
+
+public class flagflag {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  { for (int i=2;i<29;i++)
+              // {for (int a=1; a<i;a++)
+{ for (int b=1;b<i;b++)
+System.out.print("*"+" o "+"^");
+System.out.println(" ");
+}
+                // System.out.println(" ");
+                //}
+{for (int c=1;c<23;c++)
+System.out.println("*");
+}                            // this loop is used for dsesign a rod of flag
+
+//next verses are used to degigned a reversed flag
+
+{ for (int i=2;i<29;i++)
+{ for (int b=1;b<(29-i);b++)
+System.out.print("*"+" o "+"^");
+System.out.println(" ");
+}
+                // System.out.println(" ");
+                //}
+{for (int c=1;c<23;c++)
+System.out.println("*");
+}                            // this loop is used for dsesign a rod of flag
+
+
+}
+
+
+
+
+
+} 
+	}
+
+}
